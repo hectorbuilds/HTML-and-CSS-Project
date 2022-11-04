e7628e6e71fe432111e982ed3da872e9ab5718cf
hectorbuilds <115192143+hectorbuilds@users.noreply.github.com>
2022-11-03 21:49:18 -0500

    HTML and CSS assignment push
    HTML and CSS assignment push

---
+++ HelloWorld.js
@@ -0,0 +1,6 @@
+var greeting ='Hello World!';
+    consoler.log(greeting)
+    var greeting2='Welcome to Node.js'
+    console.log(greeting2);
+    var sentence1= 'Learning how to debug code with the debugger';
+    console.log(sentenceOne);
